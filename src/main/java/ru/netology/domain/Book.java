9c237c78a784852976dd6d95396a15ee3b3beb15
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-10-27 16:18:10 +0300

    Book.java added

--- src/main/java/ru/netology/domain/Book.java
+++ src/main/java/ru/netology/domain/Book.java
@@ -6,10 +6,13 @@ public class Book extends Product{
     public Book() {
         super();
     }
+
+
     public Book(int id, String name, int price, String author) {
         super(id, name, price);
         this.author = author;
     }
+
 
     public String getAuthor() {
         return author;
@@ -17,5 +20,10 @@ public class Book extends Product{
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    @Override
+    public boolean matches (String search)
+    { return super.matches(search) || author.matches(search);
     }
 }
